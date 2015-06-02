37f17f469f6821be0b4427bb7ec0a9fc1c5f6bca
makafis <vinnyalfieri@gmail.com>
2015-06-02 17:26:10 -0400

    Complete

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,8 @@
 # Build your say_hello method here
+def say_hello(person_name = "Ruby Programmer")
+
+puts "Hello " + person_name + "!"
 
 
-def say_hello(name="Ruby Programmer")
-    puts "Hello #{name}!"
 end
 
